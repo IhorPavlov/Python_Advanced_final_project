395b2ced2decd812b22b18b8b8c932e333f90167
Ihor <sgt.pavlov1911@gmail.com>
2022-02-15 21:04:12 +0200

    login

--- backend/Library/library_units/readers.py
+++ backend/Library/library_units/readers.py
@@ -1,19 +1,25 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from ..storages.base import Base
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
 
 
-class Reader(Base):
+class Reader(Base, UserMixin):
     __tablename__ = 'readers'
 
     id = Column('id', Integer, primary_key=True)
     name = Column('name', String, nullable=False)
     surname = Column('surname', String, nullable=False)
     year = Column('year', Integer, nullable=False)
+    email = Column('email', String, nullable=False)
+    psw_hash = Column('email', String, nullable=False)
 
-    def __init__(self, name: str, surname: str, year: int):
+    def __init__(self, name: str, surname: str, year: int, email: str, psw_hash: str):
         self.name = name
         self.surname = surname
         self.year = year
+        self.email = email
+        self.psw_hash= psw_hash
         # self.id = _id if _id is not None else int(id(self))
 
     def get_id(self):
@@ -27,6 +33,9 @@ class Reader(Base):
 
     def get_year(self):
         return self.year
+
+    def check_psw(self, psw: str):
+        return check_password_hash(self.psw_hash, psw)
 
     def __str__(self):
         return f'reader № {self.id}: {self.surname} {self.name}, {self.year}'
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, get_flashed_messages
 from Library.library import Library
 from Library.storages.orm_storage import ORMStorage
 import os
 from dotenv import load_dotenv
+from email_validator import validate_email
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 CURRENT_USER_ID = 1
 
@@ -14,6 +16,13 @@ app = Flask(__name__,
 
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'api_login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return lib.get_reader_by_id(user_id)
 
 storage = ORMStorage(
     db_user=os.environ.get('DB_USER'),
@@ -55,6 +64,7 @@ def api_sort_by_id():
 
 
 @app.route('/add_book', methods=['GET', 'POST'])
+@login_required
 def api_add_book():
     if request.method == 'POST':
         title_book = request.form.get('title')
@@ -62,7 +72,8 @@ def api_add_book():
         year_book = request.form.get('year')
 
         if not (title_book and author_book and year_book):
-            return render_template('add_book.html', message='Введены некорректные данные')
+            flash('Введены некорректные данные', 'Error')
+            return render_template('add_book.html')
         if not year_book.isnumeric():
             return render_template('add_book.html', message='Введен некорректный год издания')
 
@@ -87,6 +98,7 @@ def api_add_book():
 
 
 @app.route('/delete_book', methods=['GET', 'POST'])
+@login_required
 def api_delete_book():
     if request.method == 'POST':
         id_books = [int(i) for i in request.form.keys() if i.isnumeric()]
@@ -101,12 +113,13 @@ def api_delete_book():
 
 
 @app.route('/take_book', methods=['GET', 'POST'])
+@login_required
 def api_take_book():
     if request.method == 'POST':
         id_books = [int(i) for i in request.form.keys() if i.isnumeric()]
 
         if len(id_books):
-            ret_code, message = lib.books_for_reader(id_books, reader_id=1)
+            ret_code, message = lib.books_for_reader(id_books, current_user.get_id())
             # вместо reader_id = 1 вставить CURRENT_USER_ID
             return render_template('take_book.html',
                                    books=sorted(lib.get_available_books(), key=lambda book: book.id),
@@ -116,35 +129,78 @@ def api_take_book():
 
 
 @app.route('/return_book', methods=['GET', 'POST'])
+@login_required
 def api_return_book():
     if request.method == 'POST':
         id_books = [int(i) for i in request.form.keys() if i.isnumeric()]
 
         if len(id_books):
-            ret_code, message = lib.return_books_to_library(id_books, reader_id=1)
+            ret_code, message = lib.return_books_to_library(id_books, current_user.get_id())
             return render_template('return_book.html',
-                                   books=sorted(lib.get_all_book_from_reader(CURRENT_USER_ID),
+                                   books=sorted(lib.get_all_book_from_reader(current_user.get_id()),
                                                 key=lambda book: book.id),
                                    message=message)
 
     return render_template('return_book.html',
-                           books=sorted(lib.get_all_book_from_reader(CURRENT_USER_ID), key=lambda book: book.id))
+                           books=sorted(lib.get_all_book_from_reader(current_user.get_id()), key=lambda book: book.id))
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def api_registration():
-    if request.method == 'POST':
-        pass
+    if current_user.is_authentificated:
+        return redirect(url_for('index'))
 
-    return render_template('registration.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        psw = request.form.get('psw')
+        name = request.form.get('name')
+        surname = request.form.get('surname')
+        year = request.form.get('year')
+
+
+    try:
+        validate_email(email)
+    except:
+        return render_template('registration.html')
+
+    if lib.add_reader(name, surname, year, email, psw):
+        # flash('fffff')
+        return redirect(url_for('api_login'))
+    else:
+        return render_template('registration.html')
+    # return render_template('registration.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def api_login():
+    if current_user.is_authentificated:
+        return redirect(url_for('index'))
+
     if request.method == 'POST':
-        pass
+        email = request.form.get('email')
+        psw = request.form.get('psw')
+
+        if not (email and psw):
+            return  render_template('login.html', message = 'error')
+
+        reader = lib.get_reader_by_email(email)
+        if reader and reader.check_psw(psw):
+            login_user(reader)
+
+            if next_url:
+                return redirect(next_url)
+
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', message = 'error')
 
     return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+@login_required
+def api_logout():
+    if request.method == 'POST':
+        logout_user()
 
 
 if __name__ == '__main__':
